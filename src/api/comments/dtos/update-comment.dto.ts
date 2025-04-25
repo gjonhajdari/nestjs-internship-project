92a52b43a64e584522a721f8b324ec2c60eea7d3
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-04-25 12:40:04 +0200

    docs: add docs for comments DTOs (#38)

--- src/api/comments/dtos/update-comment.dto.ts
+++ src/api/comments/dtos/update-comment.dto.ts
@@ -1,8 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString, IsUUID, MaxLength } from "class-validator";
 
 export class UpdateCommentDto {
   @IsNotEmpty()
   @IsString()
   @MaxLength(150)
+  @ApiProperty({
+    type: String,
+    description: "Note contents",
+    maxLength: 150,
+    example: "This is a note about the importance of writing.",
+  })
   content: string;
 }
