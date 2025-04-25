bcc1aff1ffbe8dc81fca6730def9e7576805e5bd
Endi <148169357+EndiSalihu@users.noreply.github.com>
2025-04-25 10:33:44 +0200

    feature: create new dtos & properties for notes resource (#35)

--- src/api/notes/dtos/create-note.dto.ts
+++ src/api/notes/dtos/create-note.dto.ts
@@ -1,1 +1,29 @@
-export class CreateNoteDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsInt, IsNotEmpty, IsString, IsUUID } from "class-validator";
+
+export class CreateNoteDto {
+  @IsNotEmpty()
+  @IsUUID()
+  @ApiProperty()
+  roomId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  content: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @ApiProperty()
+  totalVotes: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  @ApiProperty()
+  xAxis: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  @ApiProperty()
+  yAxis: number;
+}
