b677d95baff3aa8bb170bd72f9be16ef623c47e9
Endi Salihu <endi.salihu03@gmail.com>
2025-04-25 11:13:43 +0200

    feature: added api documentation for notes dtos

--- src/api/notes/dtos/create-note.dto.ts
+++ src/api/notes/dtos/create-note.dto.ts
@@ -4,26 +4,50 @@ import { IsInt, IsNotEmpty, IsString, IsUUID } from "class-validator";
 export class CreateNoteDto {
   @IsNotEmpty()
   @IsUUID()
-  @ApiProperty()
+  @ApiProperty({
+    type: "string",
+    description: "A unique identifier for the room entity. This should be a valid UUID.",
+    example: "123e4567-e89b-12d3-a456-426614174000",
+  })
   roomId: string;
 
   @IsNotEmpty()
   @IsString()
-  @ApiProperty()
+  @ApiProperty({
+    type: "string",
+    description: "The content of the note. It should be a string with the note details.",
+    example: "This is a sample note content",
+    minLength: 0,
+    nullable: false,
+  })
   content: string;
 
+  //TODO: delete this
   @IsNotEmpty()
   @IsInt()
-  @ApiProperty()
   totalVotes: number;
 
   @IsNotEmpty()
   @IsInt()
-  @ApiProperty()
+  @ApiProperty({
+    type: "number",
+    description: "The X coordinate for the note on the display. It should be an integer.",
+    example: 100,
+    minimum: 0,
+    maximum: 50000,
+    nullable: false,
+  })
   xAxis: number;
 
   @IsNotEmpty()
   @IsInt()
-  @ApiProperty()
+  @ApiProperty({
+    type: "number",
+    description: "The Y coordinate for the note on the display. It should be an integer.",
+    example: 200,
+    minimum: 0,
+    maximum: 50000,
+    nullable: false,
+  })
   yAxis: number;
 }
