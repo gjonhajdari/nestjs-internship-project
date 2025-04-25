03a0c09452d6b07b6b3819b3590c99e72add9a17
DesaraQerimi <116040197+DesaraQerimi@users.noreply.github.com>
2025-04-25 11:12:25 +0200

    fix: add repository to module imports

--- src/api/rooms/rooms.module.ts
+++ src/api/rooms/rooms.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { CustomRepositoryModule } from "src/common/db/CustomRepository.module";
+import { RoomsRepository } from "./repository/rooms.repository";
 import { RoomsController } from "./rooms.controller";
 import { RoomsService } from "./rooms.service";
 
 @Module({
+  imports: [CustomRepositoryModule.forCustomRepository([RoomsRepository])],
   controllers: [RoomsController],
   providers: [RoomsService],
 })
