71cf0c2959fea6628f7c25f1ac4b19fa0e3964f4
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-04-29 14:13:31 +0200

    feature: add basic structure for notes service

--- src/api/notes/notes.module.ts
+++ src/api/notes/notes.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { CustomRepositoryModule } from "../../common/db/CustomRepository.module";
 import { NotesController } from "./notes.controller";
 import { NotesService } from "./notes.service";
-import { NoteRepository } from "./repository/note.repository";
+import { NotesRepository } from "./repository/notes.repository";
 
 @Module({
-  imports: [CustomRepositoryModule.forCustomRepository([NoteRepository])],
+  imports: [CustomRepositoryModule.forCustomRepository([NotesRepository])],
   controllers: [NotesController],
   providers: [NotesService],
+  exports: [NotesService],
 })
 export class NotesModule {}
--- src/api/notes/notes.service.ts
+++ src/api/notes/notes.service.ts
@@ -1,5 +1,39 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateNoteDto } from "./dtos/create-note.dto";
+import { UpdateNoteDto } from "./dtos/update-note.dto";
+import { Note } from "./entities/note.entity";
+import { INotesService } from "./interfaces/notes.service.interface";
+import { NotesRepository } from "./repository/notes.repository";
 
 // TODO: implement INotesService interface
 @Injectable()
-export class NotesService {}
+export class NotesService implements INotesService {
+  constructor(private notesRepository: NotesRepository) {}
+
+  async findById(noteId: string): Promise<Note> {
+    const note = await this.notesRepository.findOne({ where: { uuid: noteId } });
+
+    if (!note) throw new NotFoundException("Note does not exist");
+
+    return note;
+  }
+
+  findAll(roomId: string): Promise<Note[]> {
+    throw new Error("Method not implemented.");
+  }
+  create(createNoteDto: CreateNoteDto): Promise<Note> {
+    throw new Error("Method not implemented.");
+  }
+  updateNote(noteId: string, updateNoteDto: UpdateNoteDto): Promise<Note> {
+    throw new Error("Method not implemented.");
+  }
+  removeNote(noteId: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  addVote(noteId: string): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+  removeVote(noteId: string): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+}
