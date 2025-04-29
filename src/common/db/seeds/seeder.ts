67ca6a23f6c5c3a0b6a1ca02cbf02effb61cdd8d
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-04-29 10:41:39 +0200

    feature: add seeders for notes, rooms, and room-users

--- src/common/db/seeds/seeder.ts
+++ src/common/db/seeds/seeder.ts
@@ -2,8 +2,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { seeder } from "nestjs-seeder";
 import { DataSourceOptions } from "typeorm";
 import { config } from "../dataSource/data-source.config";
+import { NotesSeeder } from "./notes/create-notes.seed";
+import { AddToRoomSeeder } from "./rooms/add-to-room.seed";
+import { RoomsSeeder } from "./rooms/create-room.seed";
 import { UsersSeeder } from "./users/create-users.seed";
 
 seeder({
   imports: [TypeOrmModule.forRoot(config as DataSourceOptions)],
-}).run([UsersSeeder]);
+}).run([UsersSeeder, RoomsSeeder, AddToRoomSeeder, NotesSeeder]);
