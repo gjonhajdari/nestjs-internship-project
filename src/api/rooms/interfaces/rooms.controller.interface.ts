eaf4d7a2d4bf6e78fe3c509bd79b5a40975844e5
DesaraQerimi <116040197+DesaraQerimi@users.noreply.github.com>
2025-04-30 12:35:03 +0200

    feature: add interface definition for rooms

--- src/api/rooms/interfaces/rooms.controller.interface.ts
+++ src/api/rooms/interfaces/rooms.controller.interface.ts
@@ -1,2 +1,23 @@
-// biome-ignore lint/suspicious/noEmptyInterface: ToDo
-export interface IRoomsController {}
+import { IDeleteStatus } from "src/common/interfaces/DeleteStatus.interface";
+import { CreateRoomDto } from "../dtos/create-room.dto";
+import { UpdateRoomDto } from "../dtos/update-room.dto";
+import { RoomUsers } from "../entities/room-users.entity";
+import { Room } from "../entities/room.entity";
+
+export interface IRoomsController {
+  findById(roomId: string): Promise<Room>;
+
+  findRooms(): Promise<Room[]>;
+
+  create(payload: CreateRoomDto, userID: string): Room;
+
+  update(roomId: string, payload: UpdateRoomDto): Promise<Room>;
+
+  delete(roomId: string): Promise<IDeleteStatus>;
+
+  join(userId: string, roomId: string): Promise<RoomUsers>;
+
+  leave(userId: string, roomId: string): Promise<boolean>;
+
+  removeFromRoom(userId: string, roomId: string): Promise<boolean>;
+}
