36f25f3458f0e7ea06900398f24c25282e113253
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-05-05 15:26:07 +0200

    docs: add docs for users endpoint

--- src/api/user/users.controller.ts
+++ src/api/user/users.controller.ts
@@ -12,8 +12,18 @@ import {
   UsePipes,
   ValidationPipe,
 } from "@nestjs/common";
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import {
+  ApiBearerAuth,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnprocessableEntityResponse,
+} from "@nestjs/swagger";
 import { IDeleteStatus } from "src/common/interfaces/DeleteStatus.interface";
+import { DeletedResponse } from "src/common/interfaces/responses/deleted.response";
+import { NotFoundResponse } from "src/common/interfaces/responses/not-found.response";
+import { UnprocessableEntityResponse } from "src/common/interfaces/responses/unprocessable-entity.response";
 import { GetCurrentUser } from "../../common/decorators/get-current-user.decorator";
 import { Public } from "../../common/decorators/public.decorator";
 import { PermissionsGuard } from "../../common/guards/permissions.guard";
@@ -47,6 +57,18 @@ export class UsersController implements IUsersController {
   //   return await this.usersService.create(body);
   // }
 
+  @ApiOperation({
+    summary: "Get the current user",
+    description: "Retrieves the current user based on the provided token",
+  })
+  @ApiOkResponse({
+    description: "A 200 response if the user is found successfully",
+    type: User,
+  })
+  @ApiNotFoundResponse({
+    description: "A 404 error if the user doesn't exist",
+    type: NotFoundResponse,
+  })
   @Get("me")
   async getMe(@GetCurrentUser() user: User): Promise<User> {
     return await this.usersService.findOne(user.uuid);
@@ -54,6 +76,18 @@ export class UsersController implements IUsersController {
 
   // example how roles work
   // @Roles(UserRoles.SUPER_ADMIN)
+  @ApiOperation({
+    summary: "Get a user by ID",
+    description: "Retrieves a user based on the provided UUID",
+  })
+  @ApiOkResponse({
+    description: "A 200 response if the user is found successfully",
+    type: User,
+  })
+  @ApiNotFoundResponse({
+    description: "A 404 error if the user doesn't exist",
+    type: NotFoundResponse,
+  })
   @Get(":userId")
   async findOne(@Param("userId") userId: string): Promise<User> {
     return await this.usersService.findOne(userId);
@@ -66,6 +100,18 @@ export class UsersController implements IUsersController {
   //   return await this.usersService.findAll();
   // }
 
+  @ApiOperation({
+    summary: "Update the current user",
+    description: "Updates the current user based on the provided token",
+  })
+  @ApiOkResponse({
+    description: "A 200 response if the user is updated successfully",
+    type: User,
+  })
+  @ApiNotFoundResponse({
+    description: "A 404 error if the user doesn't exist",
+    type: NotFoundResponse,
+  })
   @Patch("me")
   async updateMe(@GetCurrentUser() user: User, @Body() body: UpdateUserDto) {
     return await this.usersService.updateUser(user.uuid, body);
@@ -81,6 +127,18 @@ export class UsersController implements IUsersController {
   // }
 
   // @Roles(UserRoles.SUPER_ADMIN)
+  @ApiOperation({
+    summary: "Delete the current user",
+    description: "Deletes the current user based on the provided token",
+  })
+  @ApiOkResponse({
+    description: "A 200 response if the user is deleted successfully",
+    type: DeletedResponse,
+  })
+  @ApiNotFoundResponse({
+    description: "A 404 error if the user doesn't exist",
+    type: NotFoundResponse,
+  })
   @Delete("/me")
   async deleteMe(@GetCurrentUser() user: User): Promise<IDeleteStatus> {
     return await this.usersService.deleteUser(user.uuid);
@@ -104,12 +162,34 @@ export class UsersController implements IUsersController {
   //   return this.usersService.removePermission(userId, permission);
   // }
 
+  @ApiOperation({
+    summary: "Send a password reset email",
+    description: "Sends an email to the user with a token to reset their password",
+  })
+  @ApiOkResponse({
+    description: "A 200 response if the email is sent successfully",
+  })
+  @ApiNotFoundResponse({
+    description: "A 404 error if the user doesn't exist",
+    type: NotFoundResponse,
+  })
   @Public()
   @Post("forgot")
   async forgotPassword(@Body() body: ForgotPasswordDto): Promise<void> {
     return await this.usersService.forgotPassword(body);
   }
 
+  @ApiOperation({
+    summary: "Reset the user's password",
+    description: "Resets the user's password using the provided token",
+  })
+  @ApiOkResponse({
+    description: "A 200 response if the password is reset successfully",
+  })
+  @ApiUnprocessableEntityResponse({
+    description: "A 422 error if the token is invalid or expired",
+    type: UnprocessableEntityResponse,
+  })
   @Public()
   @Post("reset/:token")
   async resetPassword(
