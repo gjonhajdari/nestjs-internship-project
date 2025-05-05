3c6b6657c75b497ebd1f56e8694cb499b15ad553
DesaraQerimi <116040197+DesaraQerimi@users.noreply.github.com>
2025-05-05 15:19:24 +0200

    docs: add api documentation for rooms controller

--- src/api/rooms/rooms.controller.ts
+++ src/api/rooms/rooms.controller.ts
@@ -8,9 +8,22 @@ import {
   Patch,
   Post,
 } from "@nestjs/common";
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from "@nestjs/swagger";
 import { GetCurrentUser } from "src/common/decorators/get-current-user.decorator";
 import { IDeleteStatus } from "src/common/interfaces/DeleteStatus.interface";
+import { BadRequestResponse } from "src/common/interfaces/responses/bad-request.response";
+import { DeletedResponse } from "src/common/interfaces/responses/deleted.response";
+import { NotFoundResponse } from "src/common/interfaces/responses/not-found.response";
+import { UnauthorizedResponse } from "src/common/interfaces/responses/unauthorized.response";
 import { User } from "../user/entities/user.entity";
 import { CreateRoomDto } from "./dtos/create-room.dto";
 import { UpdateRoomDto } from "./dtos/update-room.dto";
@@ -25,21 +38,81 @@ import { RoomsService } from "./rooms.service";
 export class RoomsController implements IRoomsController {
   constructor(private roomsService: RoomsService) {}
 
+  @ApiOperation({
+    summary: "Get one room by id",
+    description: "Retrieves the room with the specified id",
+  })
+  @ApiOkResponse({
+    description: "A 200 response if room is found",
+    type: Room,
+  })
+  @ApiUnauthorizedResponse({
+    description: "A 401 error if no bearer token is provided",
+    type: UnauthorizedResponse,
+  })
+  @ApiNotFoundResponse({
+    description: "A 404 error if the room doesn't exist",
+    type: NotFoundResponse,
+  })
   @Get(":roomId")
   async findById(@Param("roomId", new ParseUUIDPipe()) roomId: string): Promise<Room> {
     return this.roomsService.findById(roomId);
   }
 
+  @ApiOperation({
+    summary: "Get all rooms",
+    description: "Retrieves all rooms",
+  })
+  @ApiOkResponse({
+    description: "A 200 response if rooms are found successfully",
+    type: Room,
+    isArray: true,
+  })
+  @ApiUnauthorizedResponse({
+    description: "A 401 error if no bearer token is provided",
+    type: UnauthorizedResponse,
+  })
   @Get()
   async findRooms(): Promise<Room[]> {
     return this.roomsService.findRooms();
   }
 
+  @ApiOperation({
+    summary: "Create a new room",
+  })
+  @ApiCreatedResponse({
+    description: "A 201 response if the room is created successfully",
+    type: Room,
+  })
+  @ApiUnauthorizedResponse({
+    description: "A 401 error if no bearer token is provided",
+    type: UnauthorizedResponse,
+  })
   @Post()
   async create(@Body() body: CreateRoomDto, @GetCurrentUser() user: User): Promise<Room> {
     return this.roomsService.createRoom(body, user.uuid);
   }
 
+  @ApiOperation({
+    summary: "Update a room",
+    description: "Updates an existing room",
+  })
+  @ApiOkResponse({
+    description: "A 200 response if the room is updated successfully",
+    type: Room,
+  })
+  @ApiUnauthorizedResponse({
+    description: "A 401 error if no bearer token is provided",
+    type: UnauthorizedResponse,
+  })
+  @ApiNotFoundResponse({
+    description: "A 404 error if the room doesn't exist",
+    type: NotFoundResponse,
+  })
+  @ApiBadRequestResponse({
+    description: "A 400 error if the request body is invalid",
+    type: BadRequestResponse,
+  })
   @Patch(":roomId")
   async update(
     @Param("roomId", new ParseUUIDPipe()) roomId: string,
@@ -48,6 +121,22 @@ export class RoomsController implements IRoomsController {
     return this.roomsService.updateRoom(roomId, body);
   }
 
+  @ApiOperation({
+    summary: "Delete a room",
+    description: "Deletes an existing room",
+  })
+  @ApiOkResponse({
+    description: "A 200 response if the room is deleted successfully",
+    type: DeletedResponse,
+  })
+  @ApiNotFoundResponse({
+    description: "A 404 error if the room doesn't exist",
+    type: NotFoundResponse,
+  })
+  @ApiUnauthorizedResponse({
+    description: "A 401 error if no bearer token is provided",
+    type: UnauthorizedResponse,
+  })
   @Delete(":roomId")
   async delete(@Param("roomId", new ParseUUIDPipe()) roomId: string): Promise<IDeleteStatus> {
     return this.roomsService.deleteRoom(roomId);
