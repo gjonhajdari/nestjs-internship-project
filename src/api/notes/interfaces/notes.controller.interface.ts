886627329f28ca0ef6f56310a63f2afbd7bcd9e7
Endi <148169357+EndiSalihu@users.noreply.github.com>
2025-05-07 17:07:58 +0200

    refactor: update note service (#83)

--- src/api/notes/interfaces/notes.controller.interface.ts
+++ src/api/notes/interfaces/notes.controller.interface.ts
@@ -3,13 +3,13 @@ import { UpdateNoteDto } from "../dtos/update-note.dto";
 import { Note } from "../entities/note.entity";
 
 export interface INotesController {
-  getAllNotesFromRoom(roomId: string): Promise<Note[]>;
+  findAll(roomId: string): Promise<Note[]>;
 
-  createNewNote(body: CreateNoteDto): Promise<Note>;
+  create(body: CreateNoteDto): Promise<Note>;
 
-  updateNote(noteId: string, body: UpdateNoteDto): Promise<Note>;
+  update(noteId: string, body: UpdateNoteDto): Promise<Note>;
 
-  removeNote(noteId: string): Promise<void>;
+  delete(noteId: string): Promise<void>;
 
   addVote(noteId: string): Promise<boolean>;
 
