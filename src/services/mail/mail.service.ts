9086c14c97208387d807332449016fbe4aff4d6a
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-05-07 17:11:23 +0200

    refactor: update reset password url in email (#85)

--- src/services/mail/mail.service.ts
+++ src/services/mail/mail.service.ts
@@ -1,23 +1,24 @@
 import { MailerService } from "@nestjs-modules/mailer";
 import { Inject, Injectable } from "@nestjs/common";
+import { ForgotPasswordEmail } from "src/common/interfaces/ForgotPasswordEmail.interface";
 
 @Injectable()
 export class MailService {
   constructor(@Inject(MailerService) private readonly mailerService: MailerService) {}
 
   readonly fromEmail: string = process.env.SENDER_MAIL;
-  readonly FRONT_APP_URL: string = process.env.APP_URL;
+  readonly FRONT_APP_URL: string = process.env.FRONT_APP_URL;
 
-  public forgotPassword(userToken: any) {
+  public forgotPassword(userToken: ForgotPasswordEmail) {
     this.mailerService
       .sendMail({
-        to: userToken.user,
+        to: userToken.user.email,
         from: this.getFromEmail(),
         subject: this.getSubject("Reset Password"),
         template: this.getEmailTemplatePath("forgotPassword"),
         context: {
-          user: userToken.user,
-          link: `${this.FRONT_APP_URL}/user/reset/${userToken.token}`,
+          user: userToken.user.email,
+          link: `${this.FRONT_APP_URL}/reset-password?token=${userToken.token}&name=${userToken.user.firstName}`,
         },
       })
       .then((data) => data)
