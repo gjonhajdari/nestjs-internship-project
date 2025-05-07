db0c61ee2ad901c3eb0522d46d6ac5afa01e2c6c
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-05-07 17:09:05 +0200

    refactor: update refresh token route (#84)

--- src/api/auth/interfaces/auth.service.interface.ts
+++ src/api/auth/interfaces/auth.service.interface.ts
@@ -1,4 +1,5 @@
 import { LoginDto } from "../dtos/login.dto";
+import { RefreshTokenDto } from "../dtos/refresh-token.dto";
 import { RegisterDTO } from "../dtos/register.dto";
 import { Tokens } from "../types/tokens.types";
 
@@ -6,5 +7,5 @@ export interface IAuthService {
   signup: (payload: RegisterDTO) => Promise<Tokens>;
   login: (payload: LoginDto) => Promise<Tokens>;
   logout: (user: string) => Promise<void>;
-  refreshToken: (user: string, refreshToken: string) => Promise<Tokens>;
+  refreshToken: (payoad: RefreshTokenDto) => Promise<Tokens>;
 }
