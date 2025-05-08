3dd672821393804da81098ca4e5f7c226f1e7632
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-05-08 10:15:15 +0200

    docs: add documentation for refresh token DTO

--- src/api/auth/dtos/refresh-token.dto.ts
+++ src/api/auth/dtos/refresh-token.dto.ts
@@ -1,6 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class RefreshTokenDto {
+  @ApiProperty({
+    type: String,
+    description: "Refresh token for the user",
+    example:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+  })
   @IsNotEmpty()
   @IsString()
   refreshToken: string;
