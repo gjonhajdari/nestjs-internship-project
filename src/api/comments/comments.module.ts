6972790ef5f4e6324007ece44d1aaa2e012122f3
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-05-08 17:36:04 +0200

    fix: add missing UserModule dependency

--- src/api/comments/comments.module.ts
+++ src/api/comments/comments.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { CustomRepositoryModule } from "../../common/db/CustomRepository.module";
 import { NotesModule } from "../notes/notes.module";
+import { UserModule } from "../user/users.module";
 import { CommentsController } from "./comments.controller";
 import { CommentsService } from "./comments.service";
 import { CommentsRepository } from "./repository/comments.repository";
