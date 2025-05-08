6f903c7c1e40ad0943f7da5ed513480d369c5781
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-05-08 12:01:39 +0200

    docs: add documentation for auth endpoint (#87)

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -10,7 +10,23 @@ import {
   UsePipes,
   ValidationPipe,
 } from "@nestjs/common";
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from "@nestjs/swagger";
+import { BadRequestResponse } from "src/common/interfaces/responses/bad-request.response";
+import { ForbiddenResponse } from "src/common/interfaces/responses/forbidden.response";
+import { InternalErrorResponse } from "src/common/interfaces/responses/internal-error.response";
+import { LoginUserResponse } from "src/common/interfaces/responses/login-user.respons";
+import { UnauthorizedResponse } from "src/common/interfaces/responses/unauthorized.response";
+import { UserTokensResponse } from "src/common/interfaces/responses/user-tokens.response";
 import { GetCurrentUserId } from "../../common/decorators/get-current-user-id.decorator";
 import { Public } from "../../common/decorators/public.decorator";
 import { AccessTokenGuard } from "../../common/guards/access-token.guard";
@@ -29,6 +45,18 @@ import { Tokens } from "./types/tokens.types";
 export class AuthController implements IAuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    summary: "Register a new user",
+    description: "Registers a new user and returns access and refresh tokens.",
+  })
+  @ApiCreatedResponse({
+    description: "A 201 response if the user is registered successfully",
+    type: UserTokensResponse,
+  })
+  @ApiInternalServerErrorResponse({
+    description: "A 500 error if the user registration fails",
+    type: InternalErrorResponse,
+  })
   @Public()
   @Post("register")
   @HttpCode(HttpStatus.CREATED)
@@ -36,6 +64,18 @@ export class AuthController implements IAuthController {
     return await this.authService.signup(registerDto);
   }
 
+  @ApiOperation({
+    summary: "Login as a user",
+    description: "Logs in a user and returns access and refresh tokens.",
+  })
+  @ApiOkResponse({
+    description: "A 200 response if the user is logged in successfully",
+    type: LoginUserResponse,
+  })
+  @ApiBadRequestResponse({
+    description: "A 400 error if the user credentials are invalid",
+    type: BadRequestResponse,
+  })
   @Public()
   @Post("login")
   @HttpCode(HttpStatus.OK)
@@ -43,6 +83,10 @@ export class AuthController implements IAuthController {
     return this.authService.login(loginDto);
   }
 
+  @ApiOperation({
+    summary: "Log out as a user",
+    description: "Logs out a user by invalidating their refresh token.",
+  })
   @UseGuards(AccessTokenGuard)
   @Post("logout")
   @HttpCode(HttpStatus.OK)
@@ -50,6 +94,22 @@ export class AuthController implements IAuthController {
     return this.authService.logout(userId);
   }
 
+  @ApiOperation({
+    summary: "Refresh user tokens",
+    description: "Refreshes the access token using the refresh token.",
+  })
+  @ApiOkResponse({
+    description: "A 200 response if the refresh token is valid",
+    type: UserTokensResponse,
+  })
+  @ApiForbiddenResponse({
+    description: "A 403 error if the user doesn't exist or is not logged in",
+    type: ForbiddenResponse,
+  })
+  @ApiUnauthorizedResponse({
+    description: "A 401 error if the refresh token is invalid",
+    type: UnauthorizedResponse,
+  })
   @Public()
   @Post("refresh-token")
   @HttpCode(HttpStatus.OK)
