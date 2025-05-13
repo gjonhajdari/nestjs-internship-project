52856bc28fd80150ba82642ca197a20dcc8ceb7b
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-05-13 11:06:38 +0200

    fix: update log out logic in auth controller (#92)

--- src/api/auth/interfaces/auth.controller.interface.ts
+++ src/api/auth/interfaces/auth.controller.interface.ts
@@ -1,3 +1,4 @@
+import { User } from "src/api/user/entities/user.entity";
 import { LoginDto } from "../dtos/login.dto";
 import { RefreshTokenDto } from "../dtos/refresh-token.dto";
 import { RegisterDTO } from "../dtos/register.dto";
@@ -6,6 +7,6 @@ import { Tokens } from "../types";
 export interface IAuthController {
   register: (body: RegisterDTO) => Promise<Tokens>;
   login: (body: LoginDto) => Promise<Tokens>;
-  logout: (user: string) => Promise<void>;
+  logout: (user: User) => Promise<void>;
   refreshToken: (body: RefreshTokenDto) => Promise<Tokens>;
 }
