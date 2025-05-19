51d7f32840056733a116c8d3f5f48d97403332fc
Endi <148169357+EndiSalihu@users.noreply.github.com>
2025-05-19 09:27:05 +0200

    feature: add new delete note guard for notes controller (#99)

---
+++ src/common/guards/delete-note.guard.ts
@@ -0,0 +1,36 @@
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common";
+import { NotesService } from "src/api/notes/notes.service";
+import { RoomUsers } from "src/api/rooms/entities/room-users.entity";
+import { Roles } from "src/api/rooms/enums/roles.enum";
+import { User } from "src/api/user/entities/user.entity";
+import { DataSource } from "typeorm";
+
+@Injectable()
+export class DeleteNoteGuard implements CanActivate {
+  constructor(
+    private readonly notesService: NotesService,
+    private readonly dataSource: DataSource,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const user: User = request.user;
+    const noteId: string = request.params.noteId;
+
+    const note = await this.notesService.findById(noteId);
+
+    const isAuthor = note.author.id === user.id;
+
+    const roomUser = await this.dataSource.getRepository(RoomUsers).findOne({
+      where: { roomId: note.room.id, userId: user.id },
+    });
+
+    const isHost = roomUser?.role === Roles.HOST;
+
+    if (!isAuthor && !isHost) {
+      throw new ForbiddenException("You are not allowed to delete this note.");
+    }
+
+    return true;
+  }
+}
