fc0935d4eeb26b406913b47d48877c82d031ff4e
Endi Salihu <endi.salihu03@gmail.com>
2025-05-20 11:06:19 +0200

    refactor: update migration name & fix bugs

--- src/common/db/migrations/1747731076153-RemoveNoteVoteAuditEntity.ts
+++ src/common/db/migrations/1747731076153-RemoveNoteVoteAuditEntity.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class RemoveNoteVoteAudit1747729768753 implements MigrationInterface {
-  name = "RemoveNoteVoteAuditEntity1747729768753";
+export class RemoveNoteVoteAuditEntity1747731076153 implements MigrationInterface {
+  name = "RemoveNoteVoteAuditEntity1747731076153";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP INDEX "public"."IDX_fc931afdd8cfdd360bc95ab780"`);
@@ -9,6 +9,10 @@ export class RemoveNoteVoteAudit1747729768753 implements MigrationInterface {
     await queryRunner.query(`ALTER TABLE "note_votes" DROP COLUMN "created_at"`);
     await queryRunner.query(`ALTER TABLE "note_votes" DROP COLUMN "updated_at"`);
     await queryRunner.query(`ALTER TABLE "note_votes" DROP COLUMN "deleted_at"`);
+    await queryRunner.query(`ALTER TABLE "notes" ALTER COLUMN "x_axis" SET NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "notes" ALTER COLUMN "x_axis" SET DEFAULT '0'`);
+    await queryRunner.query(`ALTER TABLE "notes" ALTER COLUMN "y_axis" SET NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "notes" ALTER COLUMN "y_axis" SET DEFAULT '0'`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
@@ -25,5 +29,9 @@ export class RemoveNoteVoteAudit1747729768753 implements MigrationInterface {
     await queryRunner.query(
       `CREATE UNIQUE INDEX "IDX_fc931afdd8cfdd360bc95ab780" ON "note_votes" ("uuid") `,
     );
+    await queryRunner.query(`ALTER TABLE "notes" ALTER COLUMN "y_axis" DROP DEFAULT`);
+    await queryRunner.query(`ALTER TABLE "notes" ALTER COLUMN "y_axis" DROP NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "notes" ALTER COLUMN "x_axis" DROP DEFAULT`);
+    await queryRunner.query(`ALTER TABLE "notes" ALTER COLUMN "x_axis" DROP NOT NULL`);
   }
 }
