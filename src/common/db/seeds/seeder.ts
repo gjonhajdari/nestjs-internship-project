584ca60d9a1cfeb8446bc79b13b9022e0b2f2f07
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-05-20 16:06:32 +0200

    feature: add seeder for creating comments

--- src/common/db/seeds/seeder.ts
+++ src/common/db/seeds/seeder.ts
@@ -2,6 +2,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { seeder } from "nestjs-seeder";
 import { DataSourceOptions } from "typeorm";
 import { config } from "../dataSource/data-source.config";
+import { CommentsSeeder } from "./comments/create-comment.seeder";
 import { NotesSeeder } from "./notes/create-notes.seed";
 import { AddToRoomSeeder } from "./rooms/add-to-room.seed";
 import { RoomsSeeder } from "./rooms/create-room.seed";
@@ -9,4 +10,4 @@ import { UsersSeeder } from "./users/create-users.seed";
 
 seeder({
   imports: [TypeOrmModule.forRoot(config as DataSourceOptions)],
-}).run([UsersSeeder, RoomsSeeder, AddToRoomSeeder, NotesSeeder]);
+}).run([UsersSeeder, RoomsSeeder, AddToRoomSeeder, NotesSeeder, CommentsSeeder]);
