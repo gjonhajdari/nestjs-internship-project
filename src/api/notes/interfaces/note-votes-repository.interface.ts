9ba110faa4d3e41e56f5a7a59f7297d2542b325c
Endi Salihu <endi.salihu03@gmail.com>
2025-05-20 10:36:00 +0200

    refactor: remove audit columns from note_votes entity & delete note-vote repository

--- src/api/notes/interfaces/note-votes-repository.interface.ts
+++
@@ -1,4 +0,0 @@
-import { IBaseCustomRepository } from "../../../common/db/customBaseRepository/interfaces/BaseCustomRepository.interface";
-import { NoteVote } from "../entities/note-vote.entity";
-
-export interface INoteVoteRepository extends IBaseCustomRepository<NoteVote> {}
