ceb72a0eec417958f4497ac092a496699129dbda
DesaraQerimi <116040197+DesaraQerimi@users.noreply.github.com>
2025-05-23 16:02:18 +0200

    feature: add guard to protect private routes

---
+++ src/common/ws-guards/auth.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import * as jwt from "jsonwebtoken";
+
+@Injectable()
+export class WsAuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext) {
+    const client = context.switchToWs().getClient();
+    const token = client.handshake.headers["authorization"]?.split(" ")[1];
+
+    if (!token) return false;
+
+    try {
+      const id = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      (client as any).user = id;
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
