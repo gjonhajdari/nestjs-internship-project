c4d77d80b03dd27e2543ca8682b342fab1202bef
DesaraQerimi <116040197+DesaraQerimi@users.noreply.github.com>
2025-05-23 16:02:18 +0200

    feature: add guard to protect private routes

--- src/websocket/websocket.gateway.ts
+++ src/websocket/websocket.gateway.ts
@@ -1,3 +1,4 @@
+import { UseGuards } from "@nestjs/common";
 import {
   ConnectedSocket,
   MessageBody,
@@ -7,46 +8,44 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from "@nestjs/websockets";
-import * as jwt from "jsonwebtoken";
+import { instanceToPlain } from "class-transformer";
 import { Server, Socket } from "socket.io";
+import { CommentsService } from "src/api/comments/comments.service";
+import { CreateCommentDto } from "src/api/comments/dtos/create-comment.dto";
+import { WsAuthGuard } from "src/common/ws-guards/auth.guard";
 import { CreateNoteDto } from "../api/notes/dtos/create-note.dto";
 import { UpdateNoteDto } from "../api/notes/dtos/update-note.dto";
 import { NotesService } from "../api/notes/notes.service";
 import { RoomsService } from "../api/rooms/rooms.service";
 
 @WebSocketGateway()
+@UseGuards(WsAuthGuard)
 export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
+  private TIMEOUT = 600000;
+  private userTimeouts: Map<string, NodeJS.Timeout> = new Map();
 
   constructor(
     private roomsService: RoomsService,
     private notesService: NotesService,
+    private commentsService: CommentsService,
   ) {}
 
   handleConnection(socket: Socket) {
+    // this.resetTimeout(socket);
     console.log(`Client connected: ${socket.id}`);
-    const token = socket.handshake.headers["authorization"]?.split(" ")[1];
-    if (!token) {
-      console.warn("No token provided");
-      return socket.disconnect();
-    }
 
-    try {
-      const { id } = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-      (socket as any).user = id;
-      console.log("Connected user:", id);
-    } catch (err) {
-      console.warn("Invalid or expired token");
-      return socket.disconnect();
-    }
+    // socket.onAny(() => {
+    //   this.resetTimeout(socket);
+    // });
   }
 
   handleDisconnect(socket: Socket) {
     console.log(`Client disconnected: ${socket.id}`);
   }
 
-  @SubscribeMessage("joinRoom")
+  @SubscribeMessage("enterRoom")
   async handleJoinRoom(
     @MessageBody() data: { roomId: string },
     @ConnectedSocket() socket: Socket,
@@ -57,19 +56,19 @@ export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnec
 
       socket.join(roomId);
 
-      this.server.to(roomId).emit("userJoined", { userId });
+      this.server.to(roomId).emit("userEntered", { userId });
     } catch (error) {
       socket.emit("error", { message: "Failed to join room", detail: error.message });
     }
   }
 
-  @SubscribeMessage("newNote")
+  @SubscribeMessage("createNote")
   async handleNewNote(@MessageBody() data: CreateNoteDto, @ConnectedSocket() socket: Socket) {
     const { roomId } = data;
     try {
       const newNote = await this.notesService.createNote(data);
 
-      this.server.to(roomId).emit("newNote", { newNote });
+      this.server.to(roomId).emit("newNote", instanceToPlain(newNote));
     } catch (error) {
       socket.emit("error", { message: "Failed to create note", detail: error.message });
     }
@@ -83,7 +82,7 @@ export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnec
     const { roomId, noteId, updates } = data;
     try {
       const updatedNote = await this.notesService.updateNote(noteId, updates);
-      this.server.to(roomId).emit("updatedNote", { updatedNote });
+      this.server.to(roomId).emit("updatedNote", instanceToPlain(updatedNote));
     } catch (error) {
       socket.emit("Error in handleUpdateNote", error.message);
     }
@@ -102,4 +101,34 @@ export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnec
       socket.emit("Error in handleDeleteNote", error.message);
     }
   }
+
+  @SubscribeMessage("addComment")
+  async handleAddComment(
+    @MessageBody() data: { roomI: string; noteId: string; payload: CreateCommentDto },
+    @ConnectedSocket() socket: Socket,
+  ) {
+    const { noteId } = data;
+    const { payload } = data;
+    try {
+      const newComment = await this.commentsService.createComment(payload);
+    } catch (error) {
+      socket.emit("Error in handleAddComment", error.message);
+    }
+  }
+
+  // private resetTimeout(socket: Socket) {
+  //   this.clearTimeout(socket);
+  //   const timeout = setTimeout(() => {
+  //     socket.emit("inactive", { message: "Disconnected due to inactivity" });
+  //     console.log("Disconected due to inactivity");
+  //     socket.disconnect(true);
+  //   }, this.TIMEOUT);
+  //   this.userTimeouts.set(socket.id, timeout);
+  // }
+
+  // private clearTimeout(socket: Socket) {
+  //   const existing = this.userTimeouts.get(socket.id);
+  //   if (existing) clearTimeout(existing);
+  //   this.userTimeouts.delete(socket.id);
+  // }
 }
