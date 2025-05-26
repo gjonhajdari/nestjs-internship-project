c4983da93e5af647f398276bb2ed0dc966432ccd
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-05-26 14:16:04 +0200

    feature: add implementation for create and update endpoints (#104)

--- src/api/comments/comments.module.ts
+++ src/api/comments/comments.module.ts
@@ -1,12 +1,17 @@
 import { Module } from "@nestjs/common";
 import { CustomRepositoryModule } from "../../common/db/CustomRepository.module";
 import { NotesModule } from "../notes/notes.module";
+import { UserModule } from "../user/users.module";
 import { CommentsController } from "./comments.controller";
 import { CommentsService } from "./comments.service";
 import { CommentsRepository } from "./repository/comments.repository";
 
 @Module({
-  imports: [CustomRepositoryModule.forCustomRepository([CommentsRepository]), NotesModule],
+  imports: [
+    CustomRepositoryModule.forCustomRepository([CommentsRepository]),
+    NotesModule,
+    UserModule,
+  ],
   providers: [CommentsService],
   controllers: [CommentsController],
 })
