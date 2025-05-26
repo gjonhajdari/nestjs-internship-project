f564291e2bb831ae99da76e009e1481d3d8f1af5
DesaraQerimi <desaraqerimi@gmail.com>
2025-05-26 09:25:53 +0200

    refactor: replace absolute imports with relative paths (#100)

--- src/common/db/seeds/rooms/add-to-room.seed.ts
+++ src/common/db/seeds/rooms/add-to-room.seed.ts
@@ -1,10 +1,10 @@
 import { Injectable } from "@nestjs/common";
 import { Seeder } from "nestjs-seeder";
-import { RoomUsers } from "src/api/rooms/entities/room-users.entity";
-import { Room } from "src/api/rooms/entities/room.entity";
-import { RoomRoles } from "src/api/rooms/enums/room-roles.enum";
-import { User } from "src/api/user/entities/user.entity";
 import { In } from "typeorm";
+import { RoomUsers } from "../../../../api/rooms/entities/room-users.entity";
+import { Room } from "../../../../api/rooms/entities/room.entity";
+import { RoomRoles } from "../../../../api/rooms/enums/room-roles.enum";
+import { User } from "../../../../api/user/entities/user.entity";
 import AppDataSource from "../../dataSource/data-source.initialize";
 
 @Injectable()
