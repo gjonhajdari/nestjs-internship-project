fcfffff1fb1f227bc4577622214e6bca89c15fa7
DesaraQerimi <116040197+DesaraQerimi@users.noreply.github.com>
2025-05-28 13:32:56 +0200

    feature: add auth guard for sockets

--- src/common/ws-guards/auth.guard.ts
+++ src/common/ws-guards/auth.guard.ts
@@ -5,12 +5,12 @@ import * as jwt from "jsonwebtoken";
 export class WsAuthGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
     const client = context.switchToWs().getClient();
-    const token = client.handshake.headers["authorization"]?.split(" ")[1];
+    const token = client.handshake.auth?.Authorization?.split(" ")[1];
 
     if (!token) return false;
 
     try {
-      const id = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      const { id } = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
       (client as any).user = id;
       return true;
     } catch (error) {
