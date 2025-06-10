f85a61da979e11d45a24547e633d7de847635f41
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-06-10 15:59:32 +0200

    docs: add missing docs for verify email DTO

--- src/api/auth/dtos/verify-email.dto.ts
+++ src/api/auth/dtos/verify-email.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber, Max, Min } from "class-validator";
 
 export class VerifyEmailDto {
+  @ApiProperty({
+    type: Number,
+    description: "Verification code sent to the user's email",
+    example: 123456,
+  })
   @IsNotEmpty()
   @IsNumber({}, { message: "Code must be a number" })
   @Min(100000, { message: "Code must be 6 digits" })
