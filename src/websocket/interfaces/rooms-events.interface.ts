d51712f1a16cf3caeb209e879067a23f5d4d8638
DesaraQerimi <desaraqerimi@gmail.com>
2025-06-16 10:26:50 +0200

    feature: prevent changes in archived rooms (#125)
    Co-authored-by: Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>

--- src/websocket/interfaces/rooms-events.interface.ts
+++ src/websocket/interfaces/rooms-events.interface.ts
@@ -1,9 +1,15 @@
 export interface RoomsListenEvents {
   "rooms/join": (data: { roomId: string }) => void;
   "rooms/leave": (data: { roomId: string }) => void;
+  "rooms/archive": (data: { roomId: string }) => void;
+  "rooms/remove": (data: { roomId: string; userId: string }) => void;
+  "rooms/leaveP": (data: { roomId: string }) => void;
 }
 
 export interface RoomsEmitEvents {
   "rooms/joined": (data: { userId: string }) => void;
   "rooms/left": (data: { userId: string }) => void;
+  "rooms/archived": (data: { roomId: string }) => void;
+  "rooms/removed": (data: { userId: string }) => void;
+  "rooms/leftP": (data: { userId: string }) => void;
 }
