d3ea18b69bca083690fdb469d72199214f12510f
DesaraQerimi <116040197+DesaraQerimi@users.noreply.github.com>
2025-06-26 09:16:25 +0200

    feature: add roles guard for web sockets

--- src/websocket/rooms.gateway.ts
+++ src/websocket/rooms.gateway.ts
@@ -1,4 +1,4 @@
-import { BadRequestException } from "@nestjs/common";
+import { BadRequestException, UseGuards } from "@nestjs/common";
 import {
   ConnectedSocket,
   MessageBody,
@@ -6,11 +6,15 @@ import {
   WebSocketGateway,
 } from "@nestjs/websockets";
 import { Socket } from "socket.io";
+import { RoomRoles } from "src/api/rooms/enums/room-roles.enum";
+import { Roles } from "src/common/decorators/roles.decorator";
+import { WsRolesGuard } from "src/common/ws-guards/ws-roles.guard";
 import { ActivitiesService } from "../api/activities/activities.service";
 import { RoomsService } from "../api/rooms/rooms.service";
 import { BaseWebsocketGateway } from "./base-websocket.gateway";
 
 @WebSocketGateway()
+@UseGuards(WsRolesGuard)
 export class RoomsGateway extends BaseWebsocketGateway {
   constructor(
     private roomsService: RoomsService,
@@ -52,7 +56,7 @@ export class RoomsGateway extends BaseWebsocketGateway {
     this.server.to(roomId).emit("rooms/left", { userId: userId });
   }
 
-  //TODO: add host guard
+  @Roles(RoomRoles.HOST)
   @SubscribeMessage("rooms/archive")
   async handleArchiveRoom(
     @MessageBody() data: { roomId: string },
@@ -67,7 +71,7 @@ export class RoomsGateway extends BaseWebsocketGateway {
     }
   }
 
-  //TODO: add host guard
+  @Roles(RoomRoles.HOST)
   @SubscribeMessage("rooms/remove")
   async handleRemoveUser(
     @MessageBody() data: { roomId: string; userId: string },
