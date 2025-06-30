9cc517123e83030d94ff1192ceed6da5c08f8ff9
Endi Salihu <endi.salihu03@gmail.com>
2025-06-30 09:49:38 +0200

    refactor: update note deletion method

--- src/api/notes/notes.service.ts
+++ src/api/notes/notes.service.ts
@@ -335,9 +335,11 @@ export class NotesService implements INotesService {
       await this.dataSource.transaction(async (manager: EntityManager) => {
         const noteRepo = manager.getRepository(Note);
         const commentRepo = manager.getRepository(Comment);
+        const voteRepo = manager.getRepository(NoteVote);
 
+        await voteRepo.delete({ note });
+        await commentRepo.softDelete({ note });
         await noteRepo.softDelete({ uuid: noteId });
-        await commentRepo.softDelete({ note: note });
       });
 
       return {
