ce0de72c6a4819fc4c0116db955a2f61d0b1f7d9
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-07-03 16:05:42 +0200

    feature: add delete room in sockets (#131)
    Co-authored-by: DesaraQerimi <116040197+DesaraQerimi@users.noreply.github.com>

--- src/websocket/interfaces/rooms-events.interface.ts
+++ src/websocket/interfaces/rooms-events.interface.ts
@@ -1,9 +1,12 @@
+import { IResponseStatus } from "../../common/interfaces/ResponseStatus.interface";
+
 export interface RoomsListenEvents {
   "rooms/join": (data: { roomId: string }) => void;
   "rooms/leave": (data: { roomId: string }) => void;
   "rooms/archive": (data: { roomId: string }) => void;
   "rooms/remove": (data: { roomId: string; userId: string }) => void;
   "rooms/leaveP": (data: { roomId: string }) => void;
+  "rooms/delete": (data: { roomId: string }) => void;
 }
 
 export interface RoomsEmitEvents {
@@ -12,4 +15,5 @@ export interface RoomsEmitEvents {
   "rooms/archived": (data: { roomId: string }) => void;
   "rooms/removed": (data: { userId: string }) => void;
   "rooms/leftP": (data: { userId: string }) => void;
+  "rooms/deleted": (data: IResponseStatus) => void;
 }
