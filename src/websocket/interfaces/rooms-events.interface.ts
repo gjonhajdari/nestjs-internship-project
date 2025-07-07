269730586d09b17160325a153b8b5d7dc6e7e692
DesaraQerimi <116040197+DesaraQerimi@users.noreply.github.com>
2025-07-07 10:55:43 +0200

    feature: add room update event in ws

--- src/websocket/interfaces/rooms-events.interface.ts
+++ src/websocket/interfaces/rooms-events.interface.ts
@@ -1,9 +1,11 @@
+import { UpdateRoomDto } from "src/api/rooms/dtos/update-room.dto";
+import { Room } from "src/api/rooms/entities/room.entity";
 import { IResponseStatus } from "../../common/interfaces/ResponseStatus.interface";
 
 export interface RoomsListenEvents {
   "rooms/join": (data: { roomId: string }) => void;
   "rooms/leave": (data: { roomId: string }) => void;
-  "rooms/archive": (data: { roomId: string }) => void;
+  "rooms/update": (data: { roomId: string; payload: UpdateRoomDto }) => void;
   "rooms/remove": (data: { roomId: string; userId: string }) => void;
   "rooms/leaveP": (data: { roomId: string }) => void;
   "rooms/delete": (data: { roomId: string }) => void;
@@ -12,7 +14,7 @@ export interface RoomsListenEvents {
 export interface RoomsEmitEvents {
   "rooms/joined": (data: { userId: string }) => void;
   "rooms/left": (data: { userId: string }) => void;
-  "rooms/archived": (data: { roomId: string }) => void;
+  "rooms/updated": (data: Room) => void;
   "rooms/removed": (data: { userId: string }) => void;
   "rooms/leftP": (data: { userId: string }) => void;
   "rooms/deleted": (data: IResponseStatus) => void;
